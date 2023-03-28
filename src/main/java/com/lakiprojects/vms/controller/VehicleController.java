cc2411fe6edd6e5731d3f7b38bf2a5f74acf5c27
Lakindu999 <lakindusamarakoon@gmail.com>
2023-03-28 09:02:00 +0530

    Create Project

--- src/main/java/com/lakiprojects/vms/controller/VehicleController.java
+++ src/main/java/com/lakiprojects/vms/controller/VehicleController.java
@@ -3,10 +3,7 @@ package com.lakiprojects.vms.controller;
 import com.lakiprojects.vms.DTO.VehicleDTO;
 import com.lakiprojects.vms.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("ap1/v1/vehicle")
@@ -19,7 +16,6 @@ public class VehicleController {
     public String saveVehicle(@RequestBody VehicleDTO vehicleDTO){
         vehicleService.saveVehicle(vehicleDTO);
         return "saved";
-
     }
 
 }
